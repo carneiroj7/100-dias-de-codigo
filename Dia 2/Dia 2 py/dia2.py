cf6776e3fbdfe265ed5e937884c410de6bbddb9f
carneiroj7 <nutrijuliacarneiro@gmail.com>
2024-09-05 18:23:36 -0300

    Dia 2

---
+++ Dia 2/Dia 2 py/dia2.py
@@ -0,0 +1,7 @@
+num1 = float(input("Por favor, digite o primeiro número: "))
+num2 = float(input("Por favor, digite o segundo número"))
+
+resultado = num1 + num2 
+
+print("O resultado da soma dos dois números é igual a: ", resultado)
+
