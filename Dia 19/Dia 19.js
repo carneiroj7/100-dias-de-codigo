a0f06a8f5f7741570487dfe0fa80d17a581cf252
carneiroj7 <nutrijuliacarneiro@gmail.com>
2024-09-22 23:20:37 -0300

    Dia 19

---
+++ Dia 19/Dia 19.js
@@ -0,0 +1,24 @@
+//b845957cfc2f2f53a6bdc01d8c61d6f9
+//https://openweathermap.org/img/wn/${dados.weather[0].icon}.png
+//https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric
+
+const API = 'eb845957cfc2f2f53a6bdc01d8c61d6f9';
+
+function dadosNaTela(dados) {
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp);
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description;
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png';
+}
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${API}&lang=pt_br&units=metric`);
+
+    dadosNaTela(dados);
+}
+
+function clickButton() {
+    const cidade = document.querySelector(".input-cidade").value;
+    buscarCidade(cidade);
+}
